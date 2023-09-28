c91027c8cb8245da18796cbcd51e6f58db74e5fe
Jon Wyatt <jonnywyatt2@gmail.com>
2023-09-28 16:53:32 +0100

    CSS exercise

---
+++ src/css-modules/Two.js
@@ -0,0 +1,11 @@
+import './containers.css';
+
+export const Two = () => {
+  return <section className={'container-full-width'}>
+    <h1>Two</h1>
+    <p>Scoot takes the hassle out of urban mobility. Our bikes are placed in convenient locations in each of our cities. Use our app to locate the nearest bike, unlock it with a tap, and you’re away!</p>
+    <div>
+      <a href="#">Get Scootin</a>
+    </div>
+  </section>
+}
